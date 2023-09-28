013b3e30494c8f341c20305b7afaa16f79410ed1
llukaerjola <llukaerjola@gmail.com>
2023-09-27 18:43:01 -0700

    uploaded code

---
+++ static/js/logic.js
@@ -0,0 +1,142 @@
+
+
+//store the URL for the GeoJSON data
+const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+
+// Add a Leaflet tile layer.
+let streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+// Create a Leaflet map object.
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 3,
+    layers: [streets]
+});
+
+
+//define basemaps as the streetmap
+let baseMaps = {
+    "streets": streets
+};
+
+//define the earthquake layergroup and tectonic plate layergroups for the map
+let earthquake_data = new L.LayerGroup();
+let tectonics = new L.LayerGroup();
+
+//define the overlays and link the layergroups to separate overlays
+let overlays = {
+    "Earthquakes": earthquake_data,
+    "Tectonic Plates": tectonics
+};
+
+//add a control layer and pass in baseMaps and overlays
+L.control.layers(baseMaps, overlays).addTo(myMap);
+
+//this styleInfo function will dictate the stying for all of the earthquake points on the map
+function styleInfo(feature) {
+    return {
+        color: chooseColor(feature.geometry.coordinates[2]),
+        radius: chooseRadius(feature.properties.mag), //sets radius based on magnitude 
+        fillColor: chooseColor(feature.geometry.coordinates[2]), //sets fillColor based on the depth of the earthquake
+        fillOpacity: 1,
+        opacity: 1,
+        stroke:true,
+        weight: 1
+    }
+    
+};
+
+//define a function to choose the fillColor of the earthquake based on earthquake depth
+function chooseColor(depth) {
+    if (depth > 90) return  "#ea2c2c";
+    else if (depth > 70 ) return "#ea822c";
+    else if (depth > 50 ) return "#ee9c00";
+    else if (depth > 30 ) return "#eecc00";
+    else if (depth > 10 ) return "#d4ee00";
+    else return "#98ee00";
+};
+
+//define a function to determine the radius of each earthquake marker
+function chooseRadius(magnitude) {
+    return magnitude*5;
+};
+
+//OLD chooseRadius() function format - I realized that you can just multply magnitude by a scalar after I had walked through this
+// function chooseRadius(magnitude) {
+//     if (magnitude <= 1) return 5;
+//     else if (magnitude > 1 & magnitude <= 2) return 10;
+//     else if (magnitude > 2 & magnitude <= 3) return 15;
+//     else if (magnitude > 3 & magnitude <= 4) return 20;
+//     else if (magnitude > 4 & magnitude <= 5) return 25;
+//     else if (magnitude > 5 & magnitude <= 6) return 30;
+//     else if (magnitude > 6 & magnitude <= 7) return 35;
+//     else if (magnitude > 7 & magnitude <= 8) return 40;
+//     else return 45; //greater than mag 7 will show up as radius 70
+// };
+
+//
+d3.json(url).then(function (data) { //pull the earthquake JSON data with d3
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlon) {  //declare a point to layer function that takes a feature and latlon
+            return L.circleMarker(latlon).bindPopup("<h3>" +feature.properties.place +  feature.geometry.coordinates[2]  + feature.properties.mag ); //function creates a circleMarker at latlon and binds a popup with the earthquake id
+        },
+        style: styleInfo //style the CircleMarker with the styleInfo function as defined above
+    }).addTo(earthquake_data); // add the earthquake data to the earthquake_data layergroup / overlay
+    earthquake_data.addTo(myMap);
+
+    //this function pulls the tectonic plate data and draws a purple line over the plates
+    d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (data) { //pulls tectonic data with d3.json
+        L.geoJson(data, {
+            color: "purple",  //sets the line color to purple
+            weight: 0.5
+        }).addTo(tectonics); //add the tectonic data to the tectonic layergroup / overlay
+        tectonics.addTo(myMap);
+    });
+
+
+});
+//create legend, credit to this website for the structure: https://codepen.io/haakseth/pen/KQbjdO -- this structure is referenced in style.css
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function(myMap) {
+    var div = L.DomUtil.create("div", "legend");
+       div.innerHTML += "<h4>Depth Color Legend</h4>";
+       div.innerHTML += '<i style="background: #98ee00"></i><span>(-10 - 10)</span><br>';
+       div.innerHTML += '<i style="background:  #d4ee00"></i><span>(10- 30)</span><br>';
+       div.innerHTML += '<i style="background: #eecc00"></i><span>(30-50)</span><br>';
+       div.innerHTML += '<i style="background: orange"></i><span>(50 - 70)</span><br>';
+       div.innerHTML += '<i style="background: orangered"></i><span>(70 - 90)</span><br>';
+       div.innerHTML += '<i style="background: red"></i><span>( 90+)</span><br>';
+  
+    return div;
+  };
+  //add the legend to the map
+  legend.addTo(myMap);
+
+//scratch work for collecting the necessary  and console logging
+// //collect data with d3
+// d3.json(url).then(function (data) {
+//     console.log(data);
+//     let features = data.features;
+//     console.log(features);
+
+//     let results = features.filter(id => id.id == "nc73872510"); //replace the id string with the argument of the function once created
+//     let first_result = results[0];
+//     console.log(first_result);
+//     let geometry = first_result.geometry;
+//     console.log(geometry);
+//     let coordinates = geometry.coordinates;
+//     console.log(coordinates);
+//     console.log(coordinates[0]); // longitude
+//     console.log(coordinates[1]); // latitude
+//     console.log(coordinates[2]); // depth of earthquake
+//     let magnitude = first_result.properties.mag;
+//     console.log(magnitude);
+//     //define depth variable
+//     let depth = geometry.coordinates[2];
+//     console.log(depth);
+//     let id = first_result.id;
+//     console.log(id);
+
+//});
